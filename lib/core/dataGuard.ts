90ef408ea8aca786579055e0a85a805f77784601
Andrej Hučko <andrejhucko@pm.me>
2025-01-22 23:56:28 +0100

    add comments #2

--- lib/core/dataGuard.ts
+++ lib/core/dataGuard.ts
@@ -1,11 +1,6 @@
 import type { Expect, GuardPredicate, DataModelGuard } from '@/common/types'
 
 
-// function dataGuardF<T extends object> (predicate: GuardPredicate<T>): (o?: Expect<T>) => o is T {
-// return (dataObject?: Expect<T>): dataObject is T => predicate(dataObject)
-// }
-
-
 /**
  * Data model type guard generator
  * * Function that generates TypeScript guard function for a given TS interface.
@@ -14,13 +9,27 @@ import type { Expect, GuardPredicate, DataModelGuard } from '@/common/types'
  * * Here is the place where you can use type assertions from common
  *
  * ```
- * ...
+ * import { dataGuard } from 'ts-data-objects/core'
+ * import { isNum, isBool } from 'ts-data-objects/common'
+ * type MyData = {
+ *   name: string
+ *   age?: number
+ *   verified: boolean
+ * }
+ *
+ * // Here we define a function predicate, where `o` is the JSON object
+ * // and we expect it to contain the attributes of the types we expect
+ * const guardFunction = dataGuard<MyData>(o => (
+ *   isStr(o?.name) &&
+ *   isBool(o?.verified)
+ * ))
+ *
  * ```
  *
  * @category Core Implementation
- * @template T
- * @param predicate ...
- * @returns ...
+ * @template T - the TypeScript type the object should satisfy
+ * @param predicate - function that receives the object of expected type to perform type checks on attributes
+ * @returns typed guard function that returns `true/false` depending if it passed the predicate
  */
 export const dataGuard = <T extends object>(predicate: GuardPredicate<T>): DataModelGuard<T> =>
 	(dataObject?: Expect<T>): dataObject is T => predicate(dataObject)
