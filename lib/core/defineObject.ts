203c3bf978d8a19db44efb3cd8038100b4bb1965
Andrej Hučko <andrejhucko@pm.me>
2025-01-23 00:15:12 +0100

    WIP defineObject

--- lib/core/defineObject.ts
+++ lib/core/defineObject.ts
@@ -1,44 +1,24 @@
-import { DataModelGuard, GuardPredicate } from '@/common/types'
-import { DataConstructor } from '@/types'
 import { dataModel } from './dataModel'
 import { dataGuard } from './dataGuard'
 import { dataParser } from './dataParser'
+import { DefinedObject, DefineObjectParams, NamedConstructor, NamedGuard, NamedParser } from './types'
 
 
-export type DefineObjectParams<T extends object> = {
-	defaultValues?: T
-	predicate: GuardPredicate<T>
-	doNotThrow?: boolean
-}
-
-type NamedConstructor<Type extends object, TypeName extends string> =
-	{ [N in TypeName as `${N}`]: DataConstructor<Type> }
-
-type NamedGuard<Type extends object, TypeName extends string> =
-	{ [N in TypeName as `valid${N}`]: DataModelGuard<Type> }
-
-type NamedParser<Type extends object, TypeName extends string> =
-	{ [N in TypeName as `parse${N}`]: DataConstructor<Type> }
-
-
-export type DefinedObject<Type extends object, TypeName extends string> =
-	NamedConstructor<Type, TypeName> &
-	NamedGuard<Type, TypeName> &
-	NamedParser<Type, TypeName>
-
-
+/**
+ * * Composing function constructing functions defining, validating and parsing a JSON object to comply with given TS type.
+ *
+ * @param typeName - Name of the typed object. Required to enable partial code reflexion due to TypeScript code being removed from built bundle
+ * @param options - see {@link DefineObjectParams}
+ * @returns functions related to crafting and validating a typed data object
+ */
 export const defineObject = <Type extends object, TypeName extends string>(typeName: TypeName, options: DefineObjectParams<Type>): DefinedObject<Type, TypeName> => {
-	//
-	// ...
-	//
-
 	const __constructor = dataModel<Type>(options?.defaultValues)
 	const __namedConstructor = { [`${typeName}`]: __constructor } as NamedConstructor<Type, TypeName>
 
 	const __guard = dataGuard<Type>(options.predicate)
 	const __namedGuard = { [`valid${typeName}`]: __guard } as NamedGuard<Type, TypeName>
 
-	const __parser = dataParser<Type>(typeName, __constructor, __guard)
+	const __parser = dataParser<Type>(typeName, __constructor, __guard, options.doNotThrow)
 	const __namedParser = { [`parse${typeName}`]: __parser } as NamedParser<Type, TypeName>
 
 	return {
