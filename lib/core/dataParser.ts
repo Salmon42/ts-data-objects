20c3d8f68ccf4f7305977a903889bbbf008cefd9
Andrej Hučko <andrejhucko@pm.me>
2025-01-22 23:56:56 +0100

    add functionality for silent error, add comments

--- lib/core/dataParser.ts
+++ lib/core/dataParser.ts
@@ -4,42 +4,57 @@ import { DataConstructor } from '@/core/types'
 
 
 /**
- * ...
+ * Data parser implementation
+ * * Private implementation hidden within exported dataParser function
+ *
  * @category Core Implementation
  *
- * @param dataType
- * @param constructorFunction
- * @param guardFunction
- * @param value
- * @returns
+ * @template T - the TypeScript type the object should satisfy
+ * @param dataType - stringified name of the TS type
+ * @param constructorFunction - the function returned by `dataModel()`
+ * @param guardFunction - the function returned by `dataGuard()`
+ * @param doNotThrow - defaults to false
+ * @param value - the actual unknown JSON object
+ * @returns JSON object that has been validated and its value is either original or reconstructed depending on default values of the data object
  */
-const DataValidation = <T extends object>(
+const DPI = <T extends object>(
 	dataType: string,
 	constructorFunction: DataConstructor<T>,
 	guardFunction: DataModelGuard<T>,
+	doNotThrow: boolean,
 	value?: Expect<T>,
 ) => {
 	if (guardFunction(value)) {
 		return constructorFunction(value)
 	}
 	else {
-		throw new DataValidationError(dataType, value)
+		const error = new DataValidationError(dataType, value)
+		if (doNotThrow) {
+			console.error(error)
+		}
+		else {
+			throw error
+		}
 	}
 }
 
+
 /**
- * ...
+ * Core JSON data parser
+ *
  * @category Core Implementation
  *
- * @param dataType
- * @param constructorFunction
- * @param guardFunction
- * @returns
+ * @template T - the TypeScript type the object should satisfy
+ * @param dataType - stringified name of the TS type
+ * @param constructorFunction - the function returned by `dataModel()`
+ * @param guardFunction - the function returned by `dataGuard()`
+ * @param doNotThrow - default behavior is to throw error on incorrect validation, you can disable it here with `true`
+ * @returns parsing function that validates JSON object, otherwise throws or logs error (depending on `doNotThrow` param)
  */
 export const dataParser = <T extends object>(
 	dataType: string,
 	constructorFunction: DataConstructor<T>,
 	guardFunction: DataModelGuard<T>,
-) =>
-	(data?: Expect<T>) =>
-		DataValidation(dataType, constructorFunction, guardFunction, data)
+	doNotThrow?: boolean,
+): DataConstructor<T> =>
+	(data?: Expect<T>) => DPI(dataType, constructorFunction, guardFunction, doNotThrow ?? false, data)
